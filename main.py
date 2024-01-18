b212d5d5b9ba4a7597c13f32c7f32a16106ce263
Vincent Lagache <40307149+vlagache@users.noreply.github.com>
2024-01-18 13:23:31 +0100

    mon super squash & merge de la feature 1
    * add feature_1 function
    
    * add feature_1 improvement
    
    * add another improvement
    
    * add another improvement 2
    
    * modify print random
    
    * modify random again

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 def random_function():
     print("random ou pas et encore une autre version")
+
+def feature_1():
+    print("feature 1")
+    print("add_improvement")
+    print("add another improvement")
+    print("and another....")
